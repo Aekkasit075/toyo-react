73bb35a271e1f7746aa9dfbaf9fb1e43bf797463
KFC_PAJINKO <saharat.windows@gmail.com>
2026-07-03 11:56:49 +0700

    Add hover on card in Qa page and Annoucement page

--- components/layout/tutorialcard/card.tsx
+++ components/layout/tutorialcard/card.tsx
@@ -1,17 +1,22 @@
 import styles from './card.module.css'
+import Link from 'next/link';
 
-export default function Card({ imgsrc, text }: { imgsrc: string, text: string }) 
+export default function Card({ imgsrc, text, source }: { imgsrc: string, text: string, source: string }) 
 {
     return (
-        <div className={styles.card}>
-            <div className={styles.cardContent}>
-                <div className={styles.cardpic}>
-                    <img src={imgsrc}/>
+        <div className={styles.cardcontainer}>
+            <Link href={source}>
+                <div className={styles.card}>
+                    <div className={styles.cardContent}>
+                        <div className={styles.cardpic}>
+                            <img src={imgsrc}/>
+                        </div>
+                        <div className={styles.cardtext}>
+                            <p>{text}</p>
+                        </div>
+                    </div>
                 </div>
-                <div className={styles.cardtext}>
-                    <p>{text}</p>
-                </div>
-            </div>
+            </Link>
         </div>
     )
 }
