37ec9db18ad8495c188cf8405dc66a1dbe436ef3
KFC_PAJINKO <saharat.windows@gmail.com>
2026-07-06 17:01:49 +0700

    Common question page (not finish).

---
+++ app/commonp/page.tsx
@@ -0,0 +1,20 @@
+import styles from './page.module.css'
+import Script from 'next/script';
+import Accordion from '@/components/layout/accordion/ac';
+
+export default function Commonp()
+{
+    return(
+        <div className={styles.body}>
+            <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.3/themes/smoothness/jquery-ui.css"/>
+            <Script src="https://code.jquery.com/jquery-3.7.1.js" strategy="beforeInteractive"/>
+            <Script src="https://code.jquery.com/ui/1.13.3/jquery-ui.js" strategy="lazyOnload"/>
+            <div className={styles.title}>
+                <h1>ปัญหาที่พบบ่อย</h1>
+            </div>
+            <div className={styles.content}>
+                <Accordion title = '1.การเรียน/สอบ' info = 'กดเข้าปุ่มนู้นปุ่มนี้เดี๋ยวก็ได้เอง'/>
+            </div>
+        </div>
+    )
+}
---
+++ components/layout/accordion/ac.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import {useState} from "react";
+import styles from "./ac.module.css";
+
+// export const AccordingContext = React.createContext<{
+//     activeContent: string | null;
+//     handleTrigger: (id: string | null) => void;
+// } | null > (null);
+
+// export const AccordingItemContext = React.createContext<{
+//     id: string | null
+// } | null > (null);
+
+export default function accordion({title, info} : {title : string, info : string})
+{
+    // const [activeContent, setActiveContent] = useState<string | null >(null);
+    // const handleTrigger = (id: string | null) => {
+    //     let activeId: string | null = id;
+    //     if(activeId === activeContent) activeId = null
+    //     setActiveContent(activeId);
+    // }
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    const toggleAccordion = () => {
+        setIsOpen(!isOpen);
+    };
+    
+
+    return(
+        <div className={styles.accordion}>
+            <div className={styles.title} onClick={toggleAccordion}>
+                {title}
+                <div className={styles.info}>
+                    {isOpen ? info : ""}
+                </div>
+            </div>
+            
+        </div>
+    )
+}
