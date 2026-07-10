1f265c0abd8e621e9d53659d662042c6064addc5
KFC_PAJINKO <saharat.windows@gmail.com>
2026-07-10 14:14:33 +0700

    import Link in qapage/paget.sx

--- app/commonp/page.tsx
+++ app/commonp/page.tsx
@@ -1,12 +1,13 @@
 import styles from './page.module.css'
 import Script from 'next/script';
 import Accordion from '@/components/layout/accordion/ac';
+import Link from 'next/link'
 
 export default function Commonp()
 {
     return(
         <div className={styles.body}>
-            <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.3/themes/smoothness/jquery-ui.css"/>
+            <Link rel="stylesheet" href="https://code.jquery.com/ui/1.13.3/themes/smoothness/jquery-ui.css"/>
             <Script src="https://code.jquery.com/jquery-3.7.1.js" strategy="beforeInteractive"/>
             <Script src="https://code.jquery.com/ui/1.13.3/jquery-ui.js" strategy="lazyOnload"/>
             <div className={styles.title}>
